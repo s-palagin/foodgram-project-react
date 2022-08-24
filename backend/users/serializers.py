88e60155b50cf90f99c0b8a81e076d075e441851
Sergey Palagin <s.palagin@gmail.com>
2022-08-25 00:31:02 +0300

    Add push to server

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -8,7 +8,7 @@ from recipes.models import Recipe
 
 
 class UserSerializer(serializers.ModelSerializer):
-    is_subscribed = serializers.SerializerMethodField(read_only=True)
+    is_subscribed = serializers.SerializerMethodField()
 
     class Meta:
         fields = ('id', 'username', 'email', 'first_name', 'last_name',
